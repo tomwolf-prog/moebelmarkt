5c74d495484ffbf1cbce92653b05b4a90afa86fe
tomwolf-prog <80450849+tomwolf-prog@users.noreply.github.com>
2024-02-11 11:55:46 +0100

    Update Struktur

--- src/Moebelhaus.java
+++ src/Moebelhaus.java
@@ -1,8 +1,6 @@
 import Kategorie.*;
 import Lager.*;
-import Lager.Lager;
 
-import java.lang.*;
 
 
 public class Moebelhaus {
@@ -11,25 +9,57 @@ public class Moebelhaus {
         tisch.setHoehe(Tische.Hoehe.s);
         tisch.setMoebelart(Tische.Moebelart.Couchtisch);
 
-        Couchtischlager couchtischlager = new Couchtischlager();
-        couchtischlager.setPreis(100);
-        System.out.println(couchtischlager.getPreis());
-        Buerotischlager buerotischlager = new Buerotischlager();
-        buerotischlager.setPreis(101);
-        System.out.println(couchtischlager.getPreis());
-        System.out.println(buerotischlager.getPreis());
+        Tische tisch2 = new Tische();
+        tisch2.setHoehe(Tische.Hoehe.s);
+        tisch2.setMoebelart(Tische.Moebelart.Buerotisch);
 
-        couchtischlager.addTisch(tisch);
-        System.out.println(couchtischlager.getMoebel());
-        System.out.println(buerotischlager.getMoebel());
+        Tische tisch3 = new Tische();
+        tisch3.setHoehe(Tische.Hoehe.s);
+        tisch3.setMoebelart(Tische.Moebelart.Buerotisch);
 
-        couchtischlager.removeTisch(Tische.Hoehe.m);
-        System.out.println(couchtischlager.getMoebel());
-        System.out.println(buerotischlager.getMoebel());
+        Tische tisch4 = new Tische();
+        tisch4.setHoehe(Tische.Hoehe.s);
+        tisch4.setMoebelart(Tische.Moebelart.Buerotisch);
 
-        couchtischlager.removeTisch(Tische.Hoehe.s);
-        System.out.println(couchtischlager.getMoebel());
-        System.out.println(buerotischlager.getMoebel());
+        Sitzmoebel sitzmoebel = new Sitzmoebel();
+        sitzmoebel.setSitzplaetze(Sitzmoebel.Sitzplaetze.eins);
+        sitzmoebel.setMoebelart(Sitzmoebel.Moebelart.Fernsehcouch);
+
+        Lagersystem lagersystem = Lagersystem.getInstance();
+        lagersystem.addTisch(tisch);
+        lagersystem.addTisch(tisch2);
+        lagersystem.addTisch(tisch3);
+        lagersystem.addTisch(tisch4);
+        lagersystem.getBuerotischlager().setPreis(100);
+        lagersystem.getCouchtischlager().setPreis(101);
+        System.out.println(lagersystem.getBuerotischlager());   
+        System.out.println(lagersystem.getBuerotischlager().getMoebel());
+        System.out.println(lagersystem.getBuerotischlager().getPreis());
+
+
+        System.out.println(lagersystem.getCouchtischlager());
+        System.out.println(lagersystem.getCouchtischlager().getMoebel());
+        System.out.println(lagersystem.getCouchtischlager().getPreis());
+        // Couchtischlager couchtischlager = lagersystem.getCouchtischlager();
+        // couchtischlager.setPreis(100);
+        // System.out.println(couchtischlager.getPreis());
+        // Buerotischlager buerotischlager = new Buerotischlager();
+        // buerotischlager.setPreis(101);
+        // System.out.println(couchtischlager.getPreis());
+        // System.out.println(buerotischlager.getPreis());
+
+        // System.out.println(couchtischlager.addTisch(tisch));
+        // System.out.println(buerotischlager.addTisch(tisch));
+        // System.out.println(couchtischlager.getMoebel());
+        // System.out.println(buerotischlager.getMoebel());
+
+        // couchtischlager.removeTisch(Tische.Hoehe.m);
+        // System.out.println(couchtischlager.getMoebel());
+        // System.out.println(buerotischlager.getMoebel());
+
+        // couchtischlager.removeTisch(Tische.Hoehe.s);
+        // System.out.println(couchtischlager.getMoebel());
+        // System.out.println(buerotischlager.getMoebel());
 
     }
 }
