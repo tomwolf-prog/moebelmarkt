85c9abd196021bd8ad4091f53041d0d4a48b1f43
tomwolf-prog <80450849+tomwolf-prog@users.noreply.github.com>
2024-02-11 17:12:30 +0100

    Update Struktur

--- src/Lager/Couchtischlager.java
+++ src/Lager/Couchtischlager.java
@@ -1,16 +1,3 @@
 package Lager;
-import Kategorie.Tische;
-import Kategorie.Tische.Moebelart;
 
-public class Couchtischlager extends Tischlager{
-
-    private Moebelart moebelart = Tische.Moebelart.Couchtisch;
-
-    public boolean addTisch(Tische couchtisch) {
-        if(couchtisch.getMoebelart().equals(moebelart)){
-            moebelstueckArrayList.add(couchtisch);
-            return true;
-        }
-        return false;
-    }
-}
+public class Couchtischlager extends Tischlager{}
